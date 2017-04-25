fa78d6e4c54213485fd01bda3a518c9238a2eb29
jgwoolley <jamesxi.woolley@gmail.com>
2017-04-24 21:21:38 -0500

    Add files via upload
    revised, updated, and encorperated

---
+++ d3football/CalculatorWritter.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+public class CalculatorWritter extends FileWriter{
+	public CalculatorWritter(File file, CalculatorArray input) throws IOException, FileNotFoundException{
+		super(file);
+		if(!file.exists()){
+			file.createNewFile();
+		}
+		
+		for(int yardline = 0; yardline < CalculatorArray.yardlineSize;yardline++){
+			for(int distance = 0; distance < CalculatorArray.distanceSize;distance++){
+				for(int play = 0; play < CalculatorArray.playSize; play++){
+					CalculatorArray[yardline][distance][play] = rand.nextInt(100) - 50;// randomly generated expected points
+				}
+			}
+		}	
+		
+		
+		Integer temp = new Integer(distance[play]);
+		writer.append(temp.toString());
+		writer.append(",");
+	}           			
+}
+		writer.append(System.lineSeparator());   
+	}
+
+	
+}
+*/
